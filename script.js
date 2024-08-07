4a89a2e75f26bc701e8ef0d7eb94ff52df33f3c6
error-code <danielaisosajoseph@gmail.com>
2024-08-07 04:43:47 +0100

    Create EventListener for button
    1. Group creation of grid in a function
    2. set a default value for grid function
    3. Set a condition for users input
    4. Replace values with 'num'

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 const container = document.querySelector('#container');
-
-for (let i = 0; i < 18; i++) {
-    const squareRow = document.createElement('div');
-    squareRow.classList.add('squareRow')
-
-    for (let i = 0; i < 18; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
+const newGridButton = document.querySelector('input[type=button]');
 
 
-        squareRow.appendChild(square);
+newGridButton.addEventListener('click', () => {
+    const userInput = parseInt(prompt('Input value for new grid!', 16));
+
+    if (userInput > 0 && userInput <= 100) {
+        createGrid(userInput);
+    } else if (userInput <= 0)  {
+        alert('Value must be greater than 0');
+    } else if (userInput > 100) {
+        alert('Value must not be more than 100');
+    }
+});
+
+function createGrid(num = 18) {
+    container.innerHTML = '';
+
+    for (let i = 0; i < num; i++) {
+        const squareRow = document.createElement('div');
+
+        squareRow.classList.add('squareRow')
+        for (let i = 0; i < num; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+
+            squareRow.appendChild(square);
+        }
+
+        container.appendChild(squareRow);
     }
 
-    container.appendChild(squareRow);
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach(square => {
+        square.addEventListener('mouseover', (event) => {
+            event.target.style['background-color'] = 'rgb(120, 120, 120)';
+        })
+    });
 }
-
-const squares = document.querySelectorAll('.square');
-
-squares.forEach(square => {
-    square.addEventListener('mouseover', (event) => {
-        event.target.style['background-color'] = 'rgb(120, 120, 120)';
-    })
-});
