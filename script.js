25758aeb4bc2e7c21767381e980dcb61fa812c56
error-code <danielaisosajoseph@gmail.com>
2024-08-07 03:11:56 +0100

    Add loop for creating grid squares

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const container = document.querySelector('#container');
+
+for (let i = 0; i < 18; i++) {
+    const squareRow = document.createElement('div');
+    squareRow.classList.add('squareRow')
+
+    for (let i = 0; i < 18; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+
+
+        squareRow.appendChild(square);
+    }
+
+    container.appendChild(squareRow);
+}
